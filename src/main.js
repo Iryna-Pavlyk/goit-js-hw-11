098d6a12e6b27cde2e5007303e7c6db91fdf56e0
Iryna-Pavlyk <145845654+Iryna-Pavlyk@users.noreply.github.com>
2024-02-27 11:50:01 +0200

    add imports

--- src/main.js
+++ src/main.js
@@ -1,16 +1,8 @@
-// Описаний у документації
-import iziToast from 'izitoast';
-// Додатковий імпорт стилів
-import 'izitoast/dist/css/iziToast.min.css';
-// Описаний у документації
-import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
-import 'simplelightbox/dist/simple-lightbox.min.css';
-import octagon from './img/octagon.png';
+import { getImages } from './js/render-functions';
 
 const form = document.querySelector('.form');
 const input = document.querySelector('input[name=message]');
-const gallery = document.querySelector('.image-gallery');
+export const gallery = document.querySelector('.image-gallery');
 
 const request = {
   key: '42476589-1f9c9241784e75b96bc1ad3a5',
@@ -20,46 +12,7 @@ const request = {
   safesearch: true,
 };
 
-const select = {
-  captionsData: 'alt',
-  captionDelay: 250,
-};
-
 let URL = 'https://pixabay.com/api/?';
-const lightbox = new SimpleLightbox('.image-gallery a', select);
-
-function getImages(link) {
-  return fetch(link)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Image error!');
-      }
-      return response.json();
-    })
-    .then(data => {
-      if (data.hits.length === 0) {
-        iziToast.error({
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          messageColor: '#fafafb',
-          backgroundColor: '#ef4040',
-          theme: 'dark',
-          iconUrl: octagon,
-          iconColor: '#fafafb',
-          messageSize: '16',
-          closeOnEscape: true,
-          maxWidth: '432',
-          position: 'topRight',
-        });
-        gallery.innerHTML = '';
-      }
-      createMarkup(data);
-      lightbox.refresh();
-    })
-    .catch(error => {
-      console.log(error);
-    });
-}
 
 form.addEventListener('submit', event => {
   event.preventDefault();
@@ -69,46 +22,3 @@ form.addEventListener('submit', event => {
   input.value = '';
   getImages(URL);
 });
-
-function createMarkup(data) {
-  let markup = data.hits
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return `<li class="gallery-item">
-      <div class="image-wrapper"><a href="${largeImageURL}"><img class="image" src="${webformatURL}" alt="${tags}" width="360"
-                    height="200"></a></div>
-      <div class="text-wrapper">
-        <ul class="text-list">
-          <li class="text-list-item">
-            <h3 class="text-list-title">Likes</h3>
-            <p class="text-list-param">${likes}</p>
-          </li>
-          <li class="text-list-item">
-            <h3 class="text-list-title">Views</h3>
-            <p class="text-list-param">${views}</p>
-          </li>
-          <li class="text-list-item">
-            <h3 class="text-list-title">Comments</h3>
-            <p class="text-list-param">${comments}</p>
-          </li>
-          <li class="text-list-item">
-            <h3 class="text-list-title">Downloads</h3>
-            <p class="text-list-param">${downloads}</p>
-          </li>
-        </ul>
-      </div>
-    </li>`;
-      }
-    )
-    .join('');
-
-  gallery.innerHTML = markup;
-}
